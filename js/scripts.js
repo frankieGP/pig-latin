daa51dab0a611cd9ac4f54a91352a357eb65f475
Frankie Gavin <frankanthonygavin@gmail.com>
2016-10-26 22:48:21 -0700

    add the next few steps.

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,63 @@ $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
   event.preventDefault();
   var english = $("input#english").val();
-  var answer = finalString;
 
+  var answer = "";
   var vowels = ["a", "e", "i", "o", "u"];
   var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-  var split = english.split("");
-  var length = split.length;
 
-  var finalString = [];
-  for(var index = 0; index < length; index +=1) {
-    var character = split.shift();
-    if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
-      finalString.push(character);
-    } else {
-      finalString.push("")+ "ay";
-    }
-  }
 
-  var finalOutput = finalString.join("");
+// this is our first case, adding an ay to single vowel inputs
+//if (english.length === 1 && vowels.includes(english)) {
+  // answer = english + "ay";
+//
+var vowelStart = function(english){
+  return english + "ay";
+}
+var consonantStart = function(english){}
 
-    $("#translation").text(finalString);
-    $("#result").show();
+
+ if (vowels.includes(english[0])){
+  answer = vowelStart(english);
+
+} else if (consonants.includes(english[0])){
+  var index = 0;
+
+word = english.split("")
+  word.forEach(function(letter){
+    if (vowels.includes(letter) && index === 0)
+      index = word.indexOf(letter);
+  });
+
+
+  var firstCons = english.slice(0, index);
+  var remainder = english.slice(index);
+
+  answer = remainder + firstCons + "ay";
+}
+
+
+
+//  var answer = finalString;
+
+
+//  var split = english.split("");
+//  var length = split.length;
+
+//  var finalString = [];
+//  for(var index = 0; index < length; index +=1) {
+//    var character = split.shift();
+//    if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
+//      finalString.push(character);
+//    } else {
+//      finalString.push("")+ "ay";
+//    }
+//  }
+
+//  var finalOutput = finalString.join("");
+
+    $("#translation").text(answer);
+   $("#result").show();
 
   });
 });
